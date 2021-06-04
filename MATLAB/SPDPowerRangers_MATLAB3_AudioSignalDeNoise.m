efd03f847ec93c08df35878175f40a3036affeab
Pradyumna Nadella <85230955+PradyumnaNadella@users.noreply.github.com>
2021-06-04 16:54:19 +0530

    Add files via upload

---
+++ MATLAB/SPDPowerRangers_MATLAB3_AudioSignalDeNoise.m
@@ -0,0 +1,50 @@
+clear all;
+close all;
+clc;
+dt = 0.001;
+
+[wave,fs]=audioread('dog_x.wav',[1 4]); 
+t=(0:1/fs:(length(wave)-1)/fs);  
+figure(1)
+plot(t,wave);
+fclean=sin(2*pi*50*t)+sin(2*pi*120*t);
+f = istall(wave + 2.5*randn(size(t)));
+
+figure(2); 
+subplot(3,1,1)
+plot(t,f,'c','LineWidth',1.5)
+hold on
+plot(t,wave,'k','LineWidth',2)
+l=legend('Noisy','Clean');
+set(gca,'FontSize',16)
+
+%%% The FFT
+n = length(t);
+fhat=fft(f,n);
+PSD = fhat.*conj(fhat)/n;
+freq=1/(dt*n)*(0:n);
+L=1:floor(n/2);
+
+subplot(3,1,3);
+set(gca,'FontSize',16)
+plot(freq(L),PSD(L),'c','LineWidth',3)
+hold on
+set(gca,'FontSize',16)
+
+% using PSD to filter out the noise
+indices = PSD>100;
+PSDclean = PSD.*indices;
+fhat = indices.*fhat;
+ffilt=ifft(fhat);
+
+plot(freq(L),PSDclean(L),'-','Color',[.5 .1 0],'LineWidth',2.5)
+l1=legend('Noisy','Filtered');
+set(l1,'FontSize',16)
+
+subplot(3,1,2);
+set(gca,'FontSize',16)
+plot(t,ffilt,'-','Color',[.5 .1 0],'LineWidth',2.5)
+l1=legend('Filtered');
+set(l1,'FontSize',16)
+ylim([-10 10]);
+set(gca,'FontSize',16)
